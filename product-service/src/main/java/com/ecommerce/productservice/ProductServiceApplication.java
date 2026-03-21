a872cb64a1739b2e77f758f34ecfd136d6ddcae0
nashinnov8 <rn100204@gmail.com>
2026-03-21 07:59:11 +0700

    Test: trigger product-service CI

--- product-service/src/main/java/com/ecommerce/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/ecommerce/productservice/ProductServiceApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class  ProductServiceApplication {
 
 	public static void main(String[] args) {
+
+		// hello this is a test for git commit
 		SpringApplication.run(ProductServiceApplication.class, args);
 	}
 
